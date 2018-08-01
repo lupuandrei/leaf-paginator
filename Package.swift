0ab1746555c2ab1b674d6f3e78e7d6248266fb34
Andrei <l_cristian94@yahoo.com>
2018-08-01 13:04:50 +0300

    packages

--- Package.swift
+++ Package.swift
@@ -6,21 +6,19 @@ import PackageDescription
 let package = Package(
     name: "GFPaginator",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "GFPaginator",
             targets: ["GFPaginator"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+      .package(url: "https://github.com/vapor/vapor.git", from: "3.0.8"),
+      .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
+      .package(url: "https://github.com/vapor-community/pagination.git", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "GFPaginator",
-            dependencies: []),
+            dependencies: ["Vapor", "Leaf", "Pagination"]),
         .testTarget(
             name: "GFPaginatorTests",
             dependencies: ["GFPaginator"]),
